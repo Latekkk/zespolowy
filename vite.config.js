b5532a9d0a487e1168ed3192ae6437d648734bc5
Latekq <latek.kalisz@gmail.com>
2023-03-08 17:50:33 +0100

    Dodanie paczek i konfiguracji fajnej :)

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,32 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import laravel from 'laravel-vite-plugin'
+import react from '@vitejs/plugin-react'
+import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
+    server: {
+        hmr: {
+            host: 'localhost'
+        }
+    },
     plugins: [
         laravel({
-            input: 'resources/js/app.tsx',
+            input: ['resources/js/app.tsx'],
             refresh: true,
         }),
         react(),
+        svgr(),
     ],
-});
+    resolve: {
+        alias: {
+            '@': '/resources/js',
+            '@Svg': '/resources/svg',
+            '@Components': '/resources/js/Components',
+            '@Layouts': '/resources/js/Layouts',
+            '@Pages': '/resources/js/Pages',
+        },
+    },
+    build: {
+        sourcemap: true,
+    },
+})
