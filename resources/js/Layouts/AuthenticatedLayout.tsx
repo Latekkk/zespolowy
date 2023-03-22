84b8969b041005afa7b5538fd296215649bab6e4
Latekq <latek.kalisz@gmail.com>
2023-03-22 12:49:38 +0100

    Poprawki

--- resources/js/Layouts/AuthenticatedLayout.tsx
+++ resources/js/Layouts/AuthenticatedLayout.tsx
@@ -6,10 +6,10 @@ import ResponsiveNavLink from '@/Components/ResponsiveNavLink';
 import { Link } from '@inertiajs/react';
 import {useTranslation} from "react-i18next";
 
-export default function Authenticated({ props, header, children }) {
+export default function AuthenticatedLayout({ props, header, children }) {
     const [showingNavigationDropdown, setShowingNavigationDropdown] = useState(false);
 
-    const { t } = useTranslation(['navbar'])
+    const {t} = useTranslation(['navbar'])
 
     const auth = props.auth;
     return (
@@ -20,7 +20,7 @@ export default function Authenticated({ props, header, children }) {
                         <div className="flex">
                             <div className="shrink-0 flex items-center">
                                 <Link href="/">
-                                    <ApplicationLogo className="block h-9 w-auto fill-current text-gray-800" />
+                                    <ApplicationLogo className="block h-9 w-auto fill-current text-gray-800"/>
                                 </Link>
                             </div>
 
