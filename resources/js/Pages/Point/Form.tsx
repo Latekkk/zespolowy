a1d6ef8ca9d51850befabc03940a9d92c730c4f5
F4y3n <kubbaaa121@wp.pl>
2023-03-28 13:42:59 +0200

    punkty  tlumaczenia

--- resources/js/Pages/Point/Form.tsx
+++ resources/js/Pages/Point/Form.tsx
@@ -9,6 +9,7 @@ import Button from "@/Components/Button";
 export default function Form(props) {
 
     const {t} = useTranslation(['points'])
+    const globalTranslation = useTranslation(['global'])
     const {data, setData, post, put, processing, errors, reset, cancel, clearErrors } = useForm({
         markers: props?.point === undefined? [] : [ {'lat': Number(props?.point?.lat), 'lng': Number( props?.point?.lng)}],
         name: props?.point?.name || '',
@@ -49,22 +50,21 @@ export default function Form(props) {
 
                         <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                             <div className="flex p-6 text-gray-900 flex flex-col gap-x-2 gap-y-2">
-                                <GoogleMapComponent markers={data.markers} setMarkers={handleChange}/>
 
                                 <div className="flex flex-col gap-2 w-max">
-                                    <Input labelText={t('name')}
+                                    <Input labelText={t('point.name')}
                                            name='name'
                                            value={data.name}
                                            error={errors.name}
                                            onChange={handleChange}
                                     />
-                                    <Input labelText={t('szerokosc_geograficzna')}
+                                    <Input labelText={t('latitude')}
                                            name={'geoLat'}
                                            value={data.markers[0]?.lat ?? ''}
                                            error={errors.geoLat || errors.markers}
                                            readOnly={true}
                                     />
-                                    <Input labelText={t('długosc_geograficzna')}
+                                    <Input labelText={t('longitude')}
                                            name={'geoLng'}
                                            value={data.markers[0]?.lng ?? ''}
                                            error={errors.geoLng || errors.markers}
@@ -73,10 +73,10 @@ export default function Form(props) {
 
                                 </div>
                                 <div className={'flex flex-row gap-x-2'}>
-                                    <Button type='submit' disabled={processing} children={'submit'} background="bg-blue-500" textColor={"text-white"} hoverColor={"bg-blue-400"}/>
-                                    <Button type='button' onClick={setDefaultForm} disabled={processing} children={'reset'} background="bg-red-500" textColor={"text-white"} hoverColor={"bg-red-400"}/>
+                                    <Button type='submit' disabled={processing} children={globalTranslation.t('submit')} background="bg-blue-500" textColor={"text-white"} hoverColor={"bg-blue-400"}/>
+                                    <Button type='button' onClick={setDefaultForm} disabled={processing} children={globalTranslation.t('reset')} background="bg-red-500" textColor={"text-white"} hoverColor={"bg-red-400"}/>
 
-                                    <Button type='button' children={'cancel'} onClick={cancel}/>
+                                    <Button type='button' children={globalTranslation.t('cancel')} onClick={cancel}/>
                                 </div>
                             </div>
                         </div>
