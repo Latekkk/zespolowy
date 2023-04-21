793db5807507749fbabb7a324c99e022eaf63b5a
Weronika Graczyk <wercia01@onet.pl>
2023-04-21 19:38:43 +0200

    Create MountainPart and MountainRanges

---
+++ app/Models/MountainRanges.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @method create(array|string[] $array_merge)
+ * @method static paginate(int $int)
+ */
+class MountainRanges extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+    ];
+
+    public function getRouteKeyName(): string
+    {
+        return 'slug';
+    }
+}
