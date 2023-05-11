ad8c62f2d1e98ece16696e0f6616985dd7a3a397
Latekq <latek.kalisz@gmail.com>
2023-05-11 20:32:59 +0200

    poprawki

--- resources/js/Pages/Trip/Form.tsx
+++ resources/js/Pages/Trip/Form.tsx
@@ -1,110 +1,3 @@
-// import React, {useEffect, useState} from "react";
-// import {useTranslation} from "react-i18next";
-// import {Head, useForm} from "@inertiajs/react";
-// import {MountainSection} from "@/Models/MountainSection";
-// import Layout from "@/Layouts/Layout";
-// import DropdownWithErrorMessage from "@/Components/DropdownWithErrorMessage";
-// import Input from "@/Components/Input";
-// import Button from "@/Components/Button";
-// import MountainSectionService from "@/Pages/MountainSection/service/MountainSectionService";
-// export default function Form(props) {
-//     const {t} = useTranslation(['trip'])
-//     const globalTranslation = useTranslation(['global'])
-//
-//     const [selectedTrip, setSelectedTrip] = useState(null);
-//     const [totalPoints, setTotalPoints] = useState(null)
-//     const trip = props.trip ?? null;
-//     const [mountainSections, setMountainSections] = useState<MountainSection[]>([]);
-//
-//
-//     const {data, setData, post, put, processing, errors, reset, clearErrors } = useForm({
-//         name: trip?.name || "",
-//         totalPoints: trip?.totalPoints || "",
-//         remember: true
-//     })
-//
-//     function handleChange(e, keyName, val) {
-//         const key = e?.target?.id || keyName;
-//         let value = e?.target?.value || val || e || '';
-//         setData(data => ({
-//             ...data,
-//             [key]: value,
-//         }))
-//     }
-//
-//     function handleSubmit(e) {
-//         e.preventDefault()
-//         trip === null ?post(route('trip.store',data)): put(route('trip.update', trip.id))
-//     }
-//
-//     const setDefaultForm = () => {
-//         reset();
-//         clearErrors()
-//     }
-//
-//     useEffect(()=>{
-//         if(trip != null){
-//             setMountainSections(findMountainSection(trip.id));
-//         }
-//         getMountainSections()
-//     },[])
-//
-//     useEffect(() => {
-//         getMountainSections()
-//     }, [totalPoints]);
-//
-//     function findMountainSection(id){
-//         const result = props.mountainSections.find(mountainSections =>mountainSections.id === id);
-//         return result || null;
-//     }
-//     const getMountainSections = () => {
-//         MountainSectionService.getMountainsSections(selectedTrip?.id).then((data: MountainSection[]) => {
-//             setMountainSections(data.data);
-//         });
-//         // get mountainSection from trip
-//     }
-//
-//     return (
-//         <Layout
-//             props={props}
-//             header={<h2 className="font-semibold text-xl text-gray-800 leading-tight" >{t('creating.editing.a.trip')}</h2>}
-//         >
-//             <Head title={t('creating.editing.a.trip')}/>
-//
-//             <div className="py-12">
-//                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-//                     <form onSubmit={handleSubmit}>
-//                         <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-//                             <div className="flex p-6 text-gray-900 flex flex-col gap-x-2 gap-y-2">
-//                                 <Input labelText={t('entrance.trip.name')}
-//                                        name='name'
-//                                        value={data.name}
-//                                        error={errors.name}
-//                                        onChange={handleChange}
-//                                        placeholder={t('entrance.trip.name.ph')}
-//                                 />
-//                                 <DropdownWithErrorMessage label={t('mountainSection')}
-//                                                           value={mountainSections?.name}
-//                                                           valueTemplate={mountainSections?.name }
-//                                                           onChange={(e) => {setTotalPoints(e)}}
-//                                                           options={mountainSections}
-//                                                           optionLabel="name"
-//                                                           placeholder={t('select.a.mountainSection')}
-//                                                           className="w-full md:w-14rem"
-//                                 />
-//                                 <p> {t('trip.totalPoints')}:{totalPoints}</p>
-//                                 <div className='flex flex-row gap-x-2 w-full justify-end mt-4'>
-//                                     <Button type='button' onClick={setDefaultForm} disabled={processing} children={globalTranslation.t('reset')} background="bg-red-500" textColor={"text-white"} hoverColor={"bg-red-400"}/>
-//                                     <Button type='submit' disabled={processing} children={globalTranslation.t('submit')} background="bg-blue-500" textColor={"text-white"} hoverColor={"bg-blue-400"}/>
-//                                 </div>
-//                             </div>
-//                         </div>
-//                     </form>
-//                 </div>
-//             </div>
-//         </Layout>
-//     );
-// }
 import React, { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { Head, useForm } from "@inertiajs/react";
@@ -115,6 +8,8 @@ import DropdownWithErrorMessage from "@/Components/DropdownWithErrorMessage";
 import Input from "@/Components/Input";
 import Button from "@/Components/Button";
 import MountainSectionService from "@/Pages/MountainSection/service/MountainSectionService";
+import Trip from "@/Pages/Trip/Partials/Trip";
+import {ScrollPanel} from "primereact/scrollpanel";
 
 export default function Form(props) {
     const { t } = useTranslation(["trip"]);
@@ -264,23 +159,21 @@ export default function Form(props) {
                                     placeholder={t("select.a.mountainSection")}
                                     className="w-full md:w-14rem"
                                 />
-                                <p> {t('trip.totalPoints')}: {isNaN(totalPoints) ? 0 : totalPoints}</p>
-                                <div>
+                                <div className=''>
                                     <h3>{t("selectedMountainSections")}</h3>
-                                    {selectedMountainSections.map((section) => (
-                                        <div key={section.id}>
-                                            <span>{section.name}</span>
-                                            <Button
-                                                type="button"
-                                                onClick={() => handleRemoveMountainSection(section)}
-                                                disabled={processing}
-                                                children={globalTranslation.t("remove")}
-                                                background="bg-red-500"
-                                                textColor={"text-white"}
-                                                hoverColor={"bg-red-400"}
-                                            />
+                                    <div className="card scrollpanel-demo">
+                                        <div className="flex flex-column md:flex-row gap-5">
+                                            <div className="flex-auto">
+                                                <ScrollPanel style={{ width: '100%', height: '400px'}} >
+                                                    {selectedMountainSections.map((section) => (
+                                                        <Trip section={section} handleRemoveMountainSection={handleRemoveMountainSection}/>
+                                                    ))}
+                                                </ScrollPanel>
+                                            </div>
+
                                         </div>
-                                    ))}
+                                    </div>
+
                                 </div>
                                 <div className="flex flex-row gap-x-2 w-full justify-end mt-4">
                                     <Button
